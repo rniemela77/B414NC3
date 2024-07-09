9f7f4abcbd0fe67b3acbc1a179a5e3f2b264f981
Robert Niemela <84466763+rniemela77@users.noreply.github.com>
2024-07-09 02:25:32 +0000

    add actions

--- game3.js
+++ game3.js
@@ -1,5 +1,5 @@
-
-// Fountain
+// Description: A game where two units move around and attack each other
+import UnitHealth from "./scripts/unit-health.js";
 
 class Demo extends Phaser.Scene {
     preload() {
@@ -11,39 +11,52 @@ class Demo extends Phaser.Scene {
             {
                 action: 'moveLeft',
                 movement: -1,
+                distance: 220,
+                frequency: 0.2,
             },
             {
                 action: 'moveRight',
                 movement: 1,
+                distance: 220,
+                frequency: 0.2,
             },
             {
                 action: 'attack',
+                damage: 2,
+                frequency: 0.4
+            },
+            {
+                action: 'defend',
+                frequency: 0.1
             }
         ];
 
-        this.p1health = 100;
-        this.p2health = 100;
+        this.p1shields = this.physics.add.group();
+        this.p2shields = this.physics.add.group();
 
-        // show health
-        this.p1healthText = this.add.text(10, 10, `${this.p1health}`, { fontSize: 24 });
-        this.p2healthText = this.add.text(10, 40, `${this.p2health}`, { fontSize: 24 });
 
+        // Action history
         this.p1actionText = this.add.text(10, 70, `P1:`, { fontSize: 24 });
         this.p2actionText = this.add.text(10, 100, `P2:`, { fontSize: 24 });
 
+        // Create unit
         this.p1unit = this.physics.add.sprite(width / 2, height / 2, 'particle');
+        this.p1unit.body.setCircle(25);
+        this.p1unit.setTint(0x0000ff);
         this.p2unit = this.physics.add.sprite(width / 2, height / 2 + 100, 'particle');
+        this.p2unit.body.setCircle(25);
         this.p2unit.setTint(0xff0000);
-
-        // drag
         this.p1unit.setDrag(100);
         this.p2unit.setDrag(100);
 
-        const doAction = (unit, txt, targetHealth, unitHpTxt) => {
-            // pick a random action
-            const { action } = Phaser.Math.RND.pick(actions);
+        // Create healthbars
+        this.unitHealth = new UnitHealth(this);
+        this.unitHealth.giveHealthbars(this.p1unit, this.p2unit);
 
-            const moveDistance = 220;
+        const doAction = (unit, txt, targetHealth, unitHpTxt) => {
+            // pick a random action based on frequency
+            let action = Phaser.Math.RND.weightedPick(actions.map(a => a.action), actions.map(a => a.frequency));
+            let { distance, damage } = actions.find(a => a.action === action);
 
             let target = unit === this.p1unit ? this.p2unit : this.p1unit;
 
@@ -60,8 +73,8 @@ class Demo extends Phaser.Scene {
 
                 // move angle by 15 degrees
                 angle += action === 'moveLeft' ? 9 : -9;
-                const x = centerX + Math.cos(angle) * moveDistance;
-                const y = centerY + Math.sin(angle) * moveDistance;
+                const x = centerX + Math.cos(angle) * distance;
+                const y = centerY + Math.sin(angle) * distance;
 
                 this.tweens.add({
                     targets: unit,
@@ -75,7 +88,7 @@ class Demo extends Phaser.Scene {
             } else if (action === 'attack') {
                 newText = `*attack*`;
 
-                // create line from unit to target
+                // create red line
                 let graphics = this.add.graphics();
                 const lineTarget = { x: target.x, y: target.y };
                 graphics.lineStyle(2, 0xff0000);
@@ -84,10 +97,10 @@ class Demo extends Phaser.Scene {
                 graphics.lineTo(target.x, target.y);
                 graphics.strokePath();
                 graphics.alpha = 0;
-                graphics.setDepth(-1);              
-                
+                graphics.setDepth(-1);
 
-                //fade line in from 0 to 1
+
+                // fade red line in
                 this.tweens.add({
                     targets: graphics,
                     alpha: 1,
@@ -96,49 +109,65 @@ class Demo extends Phaser.Scene {
                     yoyo: false,
                     repeat: 0,
                     onComplete: () => {
-                        // make line white
-                        graphics.lineStyle(200, 0xffffff);
-                        graphics.beginPath();
-                        graphics.moveTo(unit.x, unit.y);
-                        graphics.lineTo(lineTarget.x, lineTarget.y);
-                        graphics.strokePath();
 
-                        // if line graphic hits target
-                        if (Phaser.Geom.Intersects.LineToRectangle(new Phaser.Geom.Line(unit.x, unit.y, lineTarget.x, lineTarget.y), target.getBounds())) {
+                        // shoot bullet
+                        let bullet = this.physics.add.sprite(unit.x, unit.y, 'particle');
+                        let angle = Phaser.Math.Angle.Between(unit.x, unit.y, target.x, target.y);
+                        let bulletSpeed = 400;
+                        bullet.setVelocity(Math.cos(angle) * bulletSpeed, Math.sin(angle) * bulletSpeed);
+                        bullet.setCollideWorldBounds(true);
+                        bullet.setScale(0.2);
 
-                            // create white circle that expands and fades out
+                        // bullet scales up and fades out
+                        this.tweens.add({
+                            targets: bullet,
+                            scale: 1.5,
+                            alpha: 0,
+                            duration: 2000,
+                            ease: 'Power2',
+                            yoyo: false,
+                            repeat: 0,
+                            onComplete: () => {
+                                bullet.destroy();
+                            }
+                        });
+
+                        // if bullet collides with shield
+                        let enemyShield = unit === this.p1unit ? this.p2shields : this.p1shields;
+                        this.physics.add.collider(bullet, enemyShield, (bullet, shield) => {
+                            bullet.destroy();
+
+                            shield.destroy();
+                        });
+
+                        // if bullet collides with target
+                        this.physics.add.collider(bullet, target, (bullet, target) => {
+                            bullet.destroy();
+
+                            if (target === this.p1unit) {
+                                this.unitHealth.p1health -= damage;
+                            } else {
+                                this.unitHealth.p2health -= damage;
+                            }
+
+                            // hit marker
                             let circle = this.add.circle(target.x, target.y, 40, 0xffffff);
                             circle.setAlpha(1);
-                            // after 0.1s
-                            this.time.addEvent({
-                                delay: 100,
-                                callback: () => {
-                                    this.tweens.add({
-                                        targets: circle,
-                                        scale: 5,
-                                        alpha: 0,
-                                        duration: 500,
-                                        ease: 'Power2',
-                                        yoyo: false,
-                                        repeat: 0,
-                                        onComplete: () => {
-                                            circle.destroy();
-                                        },
-                                    });
+                            this.tweens.add({
+                                targets: circle,
+                                scale: 1,
+                                alpha: 0,
+                                duration: 100,
+                                ease: 'Bounce.easeOut', // 'Cubic', 'Elastic', 'Bounce', 'Back
+                                yoyo: false,
+                                repeat: 0,
+                                onComplete: () => {
+                                    circle.destroy();
                                 },
-                                loop: false,
                             });
-                            
+                        });
 
-                            if (targetHealth === this.p1health) {
-                                this.p1health -= 20;
-                            } else {
-                                this.p2health -= 20;
-                            }
-                        }
-
-
-                        // fade line out
+                        // fade red line out
                         this.tweens.add({
                             targets: graphics,
                             alpha: 0,
@@ -152,48 +181,60 @@ class Demo extends Phaser.Scene {
                         });
                     }
                 });
+            }
+            // DEFEND
+            else if (action === 'defend') {
+                newText = `*defend*`;
+
+                // create shield around unit
+                let shield = this.add.circle(unit.x, unit.y, 70, 0x5555ff);
+                shield.setAlpha(0.2);
+                shield.setDepth(-1);
+                this.physics.add.existing(shield);
+                shield.body.setCircle(70);
+                shield.body.setImmovable(true);
 
 
-                return;
-                // shoot bullet from unit to target
-                // let target = unit === this.p1unit ? this.p2unit : this.p1unit;
-                // let bullet = this.physics.add.sprite(unit.x, unit.y, 'particle');
-                // let angle = Phaser.Math.Angle.Between(unit.x, unit.y, target.x, target.y);
-                // let bulletSpeed = 400;
-                // bullet.setTint(0x00ff00);
-                // bullet.setVelocity(Math.cos(angle) * bulletSpeed, Math.sin(angle) * bulletSpeed);
-                // bullet.setCollideWorldBounds(true);
-                // bullet.setScale(0.1);
-                // target.setImmovable(true);
 
-                // if bullet collides
-                this.physics.add.collider(bullet, target, (bullet, target) => {
-                    bullet.destroy();
+                // add to shield group
+                if (unit === this.p1unit) {
+                    this.p1shields.add(shield);
+                } else {
+                    this.p2shields.add(shield);
+                }
 
-                    if (targetHealth === this.p1health) {
-                        this.p1health -= 20;
-                    } else {
-                        this.p2health -= 20;
-                    }
-                });
 
-                // if bullet hits the wall
-                this.physics.world.on('worldbounds', (body) => {
-                    if (body.gameObject === bullet) {
-                        bullet.destroy();
-                    }
+                // after 2s, shield disappears
+                this.time.addEvent({
+                    delay: 1000,
+                    callback: () => {
+                        // fade shield out
+                        this.tweens.add({
+                            targets: shield,
+                            alpha: 0,
+                            // size
+                            scale: 0.2,
+                            duration: 300,
+                            ease: 'Power2',
+                            yoyo: false,
+                            repeat: 0,
+
+                            onComplete: () => {
+                                shield.destroy();
+                            }
+                        });
+                    },
+                    loop: false,
                 });
             }
 
             txt.setText(txt.text + '\n' + newText);
-
-            unitHpTxt.setText(`${targetHealth}`);
         }
 
         let turn = 0;
         // every 1s
         this.time.addEvent({
-            delay: 1000,
+            delay: 500,
             callback: () => {
                 // if (turn % 2 === 0) {
                 doAction(this.p1unit, this.p1actionText, this.p2health, this.p1healthText)
@@ -201,7 +242,7 @@ class Demo extends Phaser.Scene {
                 doAction(this.p2unit, this.p2actionText, this.p1health, this.p2healthText)
                 // }
 
-                // turn++;
+                turn++;
             },
             loop: true,
         });
@@ -226,7 +267,7 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            // debug: true,
+            debug: true,
         },
     },
     scene: Demo,
