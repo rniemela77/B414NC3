134d0ad6eb526457a78ffd452d78cddf8521104e
Robert Niemela <84466763+rniemela77@users.noreply.github.com>
2024-08-10 18:58:54 +0000

    game

--- game4.js
+++ game4.js
@@ -3,7 +3,16 @@ class Demo extends Phaser.Scene {
         this.load.image('particle', '/circle.png');
     }
 
+
     create() {
+        // this.createRisingLine();
+
+        // this.createParticleGroup();
+
+        this.createHourglassParticles();
+    }
+
+    createRisingLine() {
         // Create a line graphic
         this.graphics = this.add.graphics();
         this.graphics.lineStyle(25, 0xffffff, 1);
@@ -23,7 +32,9 @@ class Demo extends Phaser.Scene {
             duration: 15000,
             repeat: -1,
         });
+    }
 
+    createParticleGroup() {
         // Create a group of particles with physics enabled
         this.particles = this.physics.add.group({
             collideWorldBounds: true,
@@ -34,14 +45,14 @@ class Demo extends Phaser.Scene {
         // Create particles on user click
         this.input.on('pointerdown', (pointer) => {
             let elements = [
-                { name: 'air', color: 0x0000ff },
-                { name: 'earth', color: 0x00ff00 },
+                { name: 'air', color: 0xffffff },
+                { name: 'earth', color: 0x000000 },
                 { name: 'fire', color: 0xff0000 },
-                { name: 'water', color: 0xffff00 },
+                { name: 'water', color: 0x0000ff },
             ];
             let element = elements[Math.floor(Math.random() * elements.length)];
 
-            const angles = 2;
+            const angles = 4;
             for (let i = 0; i < angles; i++) {
                 this.createParticle(pointer.x, pointer.y, i * 360 / angles, element);
             }
@@ -55,28 +66,116 @@ class Demo extends Phaser.Scene {
 
             // winning matchups (air > earth > water > fire > air)
             let matchups = {
-                0x0000ff: 0x00ff00,
-                0x00ff00: 0xff0000,
-                0xff0000: 0xffff00,
-                0xffff00: 0x0000ff,
+                air: 'earth',
+                earth: 'water',
+                water: 'fire',
+                fire: 'air',
+            };
+            let matchupColors = {
+                air: 0xffffff,
+                earth: 0x000000,
+                water: 0x0000ff,
+                fire: 0xff0000,
             };
 
-            // Check if particles are of the same element
-            if (particle1.tintTopLeft === particle2.tintTopLeft) {
-                console.log('Same element');
-                return;
+            let element1 = particle1.tintTopLeft;
+            let element2 = particle2.tintTopLeft;
+
+            if (matchups[element1] === element2) {
+                particle1.setTint(matchupColors[element1]);
+                particle2.setTint(matchupColors[element2]);
+            }
+        });
+    }
+
+    createHourglassParticles() {
+        // turn off debug
+        this.physics.world.drawDebug = false;
+
+        this.gravity = 50;
+        this.particles = this.physics.add.group({
+            key: 'particle',
+            repeat: 200,
+            setXY: {
+                x: 100,
+                y: 100,
+                stepX: 50,
+                stepY: 50,
+            },
+            bounceX: 0.5,  // Set bounce on X axis
+            bounceY: 0.5,  // Set bounce on Y axis
+            // Set drag on X and Y axis
+            dragX: 5, // this makes the particles slow down over time
+            dragY: 5, // higher number = faster slow down
+
+            collideWorldBounds: true,  // Ensure particles collide with world bounds
+            gravityY: 0, // Set default gravity for all particles
+        });
+
+        this.particles.children.entries.forEach((particle, index) => {
+            particle.setScale(0.3);
+            particle.setCircle(25);
+            particle.x = width / 2 + Phaser.Math.Between(-100, 100);
+            particle.y = height / 2 + Phaser.Math.Between(-100, 100);
+            particle.setCollideWorldBounds(true);
+
+            if (index % 2 === 0) {
+                particle.setTint(0xffffff);
+                particle.body.gravity.y = this.gravity;
+            } else {
+                particle.setTint(0x000000);
+                particle.body.gravity.y = -this.gravity;
             }
 
-            // Check if particles are of the same element
-            if (matchups[particle1.tintTopLeft] === particle2.tintTopLeft) {
-                console.log('Winning matchup');
-                // particle1.destroy();
-                particle2.destroy();
-            } else {
-                console.log('Losing matchup');
-                // particle1.setTint(0x000000);
-                // particle2.setTint(0x000000);
-            }
+            // on colliding with edge, shrink the particle
+            particle.onWorldBounds = true;
+            particle.body.onWorldBounds = true;
+            particle.body.world.on('worldbounds', (body) => {
+                // make particle slightly smaller
+                body.gameObject.setScale(body.gameObject.scaleX * 0.998);
+            });
+        });
+
+        this.physics.add.collider(this.particles, this.particles);
+
+        // show timer
+        this.timeText = this.add.text(10, 10, 'Time: 0', {
+            font: '32px Arial',
+            fill: '#ffffff',
+        });
+        this.timerTime = 0;
+        this.timer = this.time.addEvent({
+            delay: 1000,
+            callback: () => {
+                this.timerTime++;
+                this.timeText.setText('Time: ' + this.timerTime);
+                // this.gravity *= 0.9;
+                if (this.gravity < 50) {
+                    // this.gravity = 50;
+                }
+                this.particles.children.entries.forEach((particle, index) => {
+                    if (index % 2 === 0) {
+                        particle.body.gravity.y = this.gravity;
+                    } else {
+                        particle.body.gravity.y = -this.gravity;
+                    }
+
+                    if (particle.scaleX < 0.1) {
+                        // particle.destroy();
+
+                        // restore the particle
+                        return;
+                        particle.x = width / 2;
+                        particle.y = height / 2;
+                        particle.scaleX = 0.5;
+                        particle.scaleY = 0.5;
+                    }
+                    // add 1 particle
+                    // this.particles.create(particle.x, particle.y, 'particle').setScale(0.5).setCircle(25).setCollideWorldBounds(true);
+                });
+            },
+            callbackScope: this,
+            loop: true,
         });
     }
 
@@ -84,9 +183,9 @@ class Demo extends Phaser.Scene {
         // Create a particle
         let particle = this.particles.create(x, y, 'particle');
         // move them slightly apart
-        particle.x += Math.cos(angle * Math.PI / 180) * 25;
-        particle.y += Math.sin(angle * Math.PI / 180) * 25;
-        particle.setScale(1);
+        particle.x += Math.cos(angle * Math.PI / 180) * 50;
+        particle.y += Math.sin(angle * Math.PI / 180) * 50;
+        particle.setScale(1.5);
         particle.setCircle(25);
         particle.setTint(element.color);
 
